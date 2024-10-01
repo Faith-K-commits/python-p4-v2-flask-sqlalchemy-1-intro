ea7494411139ae4aead9d381dbfe1be573176996
Faith Kariuki <tcwamboi@gmail.com>
2024-10-01 14:41:49 +0300

    Complete lab

--- server/app.py
+++ server/app.py
@@ -20,6 +20,21 @@ migrate = Migrate(app, db)
 # initialize the Flask application to use the database
 db.init_app(app)
 
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True,
+                         nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
